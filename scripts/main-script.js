552b1d1d829a0de9236d2a0240d759dab60b2360
Javier Lopez <javierlopez_85@hotmail.com>
2022-03-15 02:28:15 -0300

    carousels added, footer fixed

---
+++ scripts/main-script.js
@@ -0,0 +1,31 @@
+$('.best-seller-carousel').owlCarousel({
+    loop:true,
+    center: true,
+    nav: false,
+    dots: false,
+    margin:40,
+    responsiveClass:true,
+    responsiveBaseElement: '.best-seller-carousel-wrapper',
+
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+$('.testimonial-carousel').owlCarousel({
+    loop: true,
+    center: true,
+    nav: true,
+    dots: false,
+    margin: 30,
+    items: 1,
+    navText:['<i class="fa-solid fa-caret-left"></i>', '<i class="fa-solid fa-caret-right"></i>']
+})
